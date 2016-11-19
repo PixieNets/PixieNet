e1029cb3699af21ead4a188d0e99361b26fc4c54
Esha Uboweja <euboweja@gmail.com>
2016-11-19 14:12:27 -0500

    Fixing bit access in characters for XNOR

--- Prototype/BinaryMatrix.cpp
+++ Prototype/BinaryMatrix.cpp
@@ -40,6 +40,19 @@ std::pair<int, int> elem_accessor(int i, int rows, int cols, bool transposed) {
     }
 }
 
+char get_bit(char elem, int bit_id) {
+    char mask = 1 << (8 - bit_id);
+    return (elem & mask) >> (8 - bit_id);
+}
+
+char set_bit(char elem, int bit_id, char bit) {
+    // assumes originally the bit is set to 0
+    char mask = 1 << (8 - bit_id);
+    char new_elem = (elem | (bit << mask));
+    return new_elem;
+}
+
+
 BinaryMatrix BinaryMatrix::tBinMultiply(const BinaryMatrix& other) {
     int w = this->width;
     int h = this->height;
@@ -53,8 +66,14 @@ BinaryMatrix BinaryMatrix::tBinMultiply(const BinaryMatrix& other) {
     for (int bit_id = 0; bit_id < (w * h); ++bit_id) {
         std::pair<int, int> this_rc = elem_accessor(bit_id, this_n, 8 this->transposed);
         std::pair<int, int> other_rc = elem_accessor(bit_id, other_n, 8, other.transposed);
-        res.data[bit_id] = this->data[this_rc.first][this_rc.second]
-                        * other.data[other_rc.first][other_rc.second];
+        std::pair<int, int> res_rc = elem_accessor(bit_Id, res.dataLength, 8, res.transposed);
+        char this_c = this->data[this_rc.first];
+        char other_c = other.data[other_rc.first];
+        char res_c = res.data[res_rc.first];
+        
+        char answer = (!(get_bit(this_c, this_rc.second) ^ get_bit(other_c, other_rc.second)))
+                        & 0x0001;
+        res.data[res_rc.first] = set_bit(res_c, res_rc.second, answer);
     }
     return res;
 }
