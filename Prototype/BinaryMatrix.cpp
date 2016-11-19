99a4292b81e3a081488981098a6079c028ebef39
Esha Uboweja <euboweja@gmail.com>
2016-11-19 13:48:14 -0500

    Accessor returns row, column, using that in transpose multiplication

--- Prototype/BinaryMatrix.cpp
+++ Prototype/BinaryMatrix.cpp
@@ -50,11 +50,11 @@ BinaryMatrix BinaryMatrix::tBinMultiply(const BinaryMatrix& other) {
     int this_n = this->dataLength;
     int other_n = other.dataLength;
     BinaryMatrix res(w, h);
-    int this_bit_id = 0, other_bit_id = 0;
     for (int bit_id = 0; bit_id < (w * h); ++bit_id) {
-        this_bit_id = elem_accessor(bit_id, this_n, 8 this->transposed);
-        other_bit_id = elem_accessor(bit_id, other_n, 8, other.transposed);
-        res.data[bit_id] = this->data[this_bit_id] * other.data[other_bit_id];
+        std::pair<int, int> this_rc = elem_accessor(bit_id, this_n, 8 this->transposed);
+        std::pair<int, int> other_rc = elem_accessor(bit_id, other_n, 8, other.transposed);
+        res.data[bit_id] = this->data[this_rc.first][this_rc.second]
+                        * other.data[other_rc.first][other_rc.second];
     }
     return res;
 }
