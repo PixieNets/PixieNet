e92b794e69a424e8c1dbb311290c0ca96669fa0b
Esha Uboweja <euboweja@gmail.com>
2016-11-21 21:42:31 -0500

    Added Armadillo in CMake + test in main

--- Prototype/main.cpp
+++ Prototype/main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <armadillo>
 
 #include "TestBinaryMatrix.h"
 
+using namespace arma;
+
 int main() {
+
+    // Test armadillo loading
+    mat A = randu<mat>(4,5);
+    mat B = randu<mat>(4,5);
+
+    std::cout << "Multiplying two random 4 x 5 matrices using Armadillo:\n";
+    std::cout << A * B.t() << std::endl;
+
     TestBinaryMatrix test;
     test.runAllTests();
+
     return 0;
 }
